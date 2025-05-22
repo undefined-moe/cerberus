b36f0606ea418188a68c1128d094513859515c76
LightQuantum <self@lightquantum.me>
2025-05-22 16:27:18 -0400

    chore: bump version to v0.4.3

--- core/const.go
+++ core/const.go
@@ -6,6 +6,6 @@ const (
 	AppName    = "cerberus"
 	VarIPBlock = "cerberus-block"
 	VarReqID   = "cerberus-request-id"
-	Version    = "v0.4.2"
+	Version    = "v0.4.3"
 	NonceTTL   = 2 * time.Minute
 )
